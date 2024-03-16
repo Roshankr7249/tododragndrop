5ac52e22d7725dba3a82d896f125a9b652901cee
Roshan_Kumar <mindsspiritual0@gmail.com>
2024-03-17 01:05:30 +0530

    drag and drop

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,20 @@
 import { faBars, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+// import React, { useState } from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
+
 import "./App.css";
 
 const grid = 8;
+
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 const getItemStyle = (isDragging, draggableStyle) => ({
   userSelect: "none",
@@ -19,11 +29,11 @@ const getListStyle = (isDraggingOver) => ({
   background: isDraggingOver ? "white" : "white",
   padding: grid,
   gap: "10px",
-//   backgroundColor: "lightgrey",
+  //   backgroundColor: "lightgrey",
   width: 250,
 });
 
-const TodoList = ({  items, droppableId }) => (
+const TodoList = ({ items, droppableId }) => (
   <Droppable droppableId={droppableId}>
     {(provided, snapshot) => (
       <div
@@ -31,32 +41,39 @@ const TodoList = ({  items, droppableId }) => (
         style={getListStyle(snapshot.isDraggingOver)}
         {...provided.droppableProps}
       >
+        <p>...</p>
+        {/* <MoreHorizontal/> */}
         {items.map((item, index) => (
           <Draggable key={item.id} draggableId={item.id} index={index}>
             {(provided, snapshot) => (
-                
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                style={getItemStyle(
-                  snapshot.isDragging,
-                  provided.draggableProps.style
-                )}
-                className="todo-item"
-              >
-                
-                <div className="todo-content">
-                  {item.content}
-                  <div className="icons">
-                    <FontAwesomeIcon icon={faBars} className="hamburger-icon" />
-                    <FontAwesomeIcon
-                      icon={faEnvelope}
-                      className="message-icon"
-                    />
+              <div className="header">
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                  )}
+                  className="todo-item"
+                >
+                  <div className="todo-content">
+                    <div className="line" style={{ color: getRandomColor() }}>
+                      -
+                    </div>
+                    {item.content}
+                    <div className="icons">
+                      <FontAwesomeIcon
+                        icon={faBars}
+                        className="hamburger-icon"
+                      />
+                      <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className="message-icon"
+                      />
+                    </div>
                   </div>
                 </div>
-                
               </div>
             )}
           </Draggable>
